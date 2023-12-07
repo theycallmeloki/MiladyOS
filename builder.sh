373072475fb10147f6dd8a4f85f40dbeeabcdea2
Loki <dude.1996@live.com>
2023-12-08 02:12:18 +0530

    LGTM!

--- builder.sh
+++ builder.sh
@@ -2,5 +2,5 @@ docker stop miladyos
 docker rm miladyos
 docker rmi ogmiladyloki/miladyos
 docker buildx build --platform linux/amd64,linux/arm64 -t ogmiladyloki/miladyos --push .
-docker run -d --name miladyos --privileged --user root --restart=unless-stopped --net=host --env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=password -v /var/run/docker.sock:/var/run/docker.sock ogmiladyloki/miladyos
+docker run -d --name miladyos --privileged --user root --restart=unless-stopped --net=host --env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=password --env API_TOKEN=reallythisisthepasswordareyousure -v /var/run/docker.sock:/var/run/docker.sock ogmiladyloki/miladyos
 docker ps
