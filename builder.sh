8f735e55a0bde94700b9d77615abc45f3498fcdd
Loki San <ogmiladyloki@gmail.com>
2024-07-12 18:03:54 +0530

    LGTM!

--- builder.sh
+++ builder.sh
@@ -3,6 +3,6 @@ docker rm miladyos
 docker rmi ogmiladyloki/miladyos 
 docker build -t ogmiladyloki/miladyos . 
 docker push ogmiladyloki/miladyos 
-docker run -d --name miladyos --privileged --user root --restart=unless-stopped --net=host --env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=password -v /var/run/docker.sock:/var/run/docker.sock ogmiladyloki/miladyos
+docker run --gpus all -d --name miladyos --privileged --user root --restart=unless-stopped --net=host --env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=password -v /var/run/docker.sock:/var/run/docker.sock ogmiladyloki/miladyos
 docker ps
 docker logs --follow miladyos
